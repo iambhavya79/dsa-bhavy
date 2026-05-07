de8eafbc0fd7e227795888bb7a7e2af54c290593
iambhavya79 <iambhavya79@gmail.com>
2026-05-07 20:38:44 +0530

    add condition

---
+++ basic/condition.java
@@ -0,0 +1,23 @@
+package basic;
+
+import java.util.Scanner;
+
+public class condition {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int button = sc.nextInt();
+
+
+            if (button == 1) {
+                System.out.println("amit");
+            } else if (button == 2) {
+                System.out.println("anuj");
+            } else if (button == 3) {
+                System.out.println("rishu ");
+            } else {
+                System.out.println("bhavy");
+            }
+        }
+
+    
+}
