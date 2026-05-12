1047893a01848b3348f99ab326c5d63f5c59e317
iambhavya79 <iambhavya79@gmail.com>
2026-05-13 00:36:51 +0530

    normal commit

--- array/kadane_maxsubarray_.java
+++ array/kadane_maxsubarray_.java
@@ -1,21 +1,38 @@
 package array;
 
 public class kadane_maxsubarray_ {
-    public static void kadane(int arr[]){
+
+    public static void kadane(int arr[]) {
+
         int cs = 0;
         int ms = Integer.MIN_VALUE;
-        for(int i = 0; i < arr.length; i++){
+        int largest = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+
+            largest = Math.max(largest, arr[i]);
+
             cs = cs + arr[i];
-            if(cs<0){
+
+            if (cs < 0) {
                 cs = 0;
             }
-            ms = Math.max(ms, cs);
 
-        }System.out.println(ms);
+            ms = Math.max(ms, cs);
+        }
+
+        // print only once
+        if (ms == 0) {
+            System.out.println(largest);
+        } else {
+            System.out.println(ms);
+        }
     }
+
     public static void main(String[] args) {
-        int arr[] = {-2,3,4,-1,2,5,-6};
+
+        int arr[] = {-3, -4, -5, -6, -7};
+
         kadane(arr);
-    
     }
 }
