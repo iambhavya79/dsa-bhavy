16cfb0347481fef627d595a4296e58e048f3ae20
iambhavya79 <iambhavya79@gmail.com>
2026-06-28 17:48:27 +0530

    NORMAL COMMIT

---
+++ array/trapped_water.java
@@ -0,0 +1,29 @@
+package array;
+
+public class trapped_water {
+    public static int Totaltrappedwater(int hieght[]){
+        int n = hieght.length;
+        int leftmax[] = new int [n];
+         leftmax[0] = hieght[0];
+         for(int i = 1 ; i<n; i++){
+            leftmax[i] = Math.max(hieght[i] , leftmax[i-1]);
+         }
+        int rightmax[] = new int [n];
+        rightmax[n-1] = hieght[n-1];
+        for(int i = n-2; i>=0 ;i--){
+            rightmax[i] = Math.max(hieght[i],rightmax[i+1]);
+        }
+        int trappedwater = 0;
+        for(int i = 0  ; i<n;i++ ){
+          int watertrapped = Math.min(leftmax[i] , rightmax[i]);
+
+          trappedwater += watertrapped - hieght[i] ; 
+        }
+          return trappedwater;
+        }public static void main(String[] args) {
+            int hieght[] = {4,2,0,6,3,2,5};
+            System.out.println(Totaltrappedwater(hieght));
+        }
+    }
+    
+
